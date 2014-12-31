df70bab6a05ae28b208dbcfd0537f0e1fbbd74a0
Aaron Lehrian <aaron.lehrian@gmail.com>
2014-12-30 19:42:44 -0500

    Added some error checks
    Tries to keep the bot running 24/7, unless the user sends a keyboard
    interrupt. Will add more robust error checking later (or never)

--- main.py
+++ main.py
@@ -48,46 +48,53 @@ def update_config():
 ## Main program loop. Should never exit except for emergencies
 ##
 def bot_loop():
-	global reply_count, already_done, last_checked
+		global reply_count, already_done, last_checked
 
-	print "Running bot."
-	while True:
+		print "Running bot."
+		while True:
+			try:
+				for subreddit in subreddits:
+					s = r.get_subreddit(subreddit)
+					comments = s.get_comments(limit=50)
+					first = True
+					for comment in comments:
+						if first:
+							first_comment = comment.id
+							first = False
+						if comment.id == last_checked[subreddit]:
+							break
+						for text in match_text:
+		#					print "checking " + text + " in " + comment.body.lower() + " in " + subreddit
+							if text in comment.body.lower() and comment.id not in already_done:
+								print "Match! Replying to " + comment.author.name
+								try:
+									reply = comment.reply(reply_text) #TODO: implement buffer, and check for ratelimit exceded
+								except praw.errors.RateLimitExceeded as error:
+									print "Waiting " + str(error.sleep_time) + " seconds due to rate limit..."
+									time.sleep(error.sleep_time)
+									print "Trying again..."
+									reply = comment.reply(reply_text)
+								already_done.add(comment.id)
+								print "Done. See the comment here: " + reply.permalink
+								reply_count += 1
+								print "Bot count: " + str(reply_count)
+					last_checked[subreddit] = first_comment
 
-		for subreddit in subreddits:
-			s = r.get_subreddit(subreddit)
-			comments = s.get_comments(limit=50)
-			first = True
-			for comment in comments:
-				if first:
-					first_comment = comment.id
-					first = False
-				if comment.id == last_checked[subreddit]:
-					break
-				for text in match_text:
-#					print "checking " + text + " in " + comment.body.lower() + " in " + subreddit
-					if text in comment.body.lower() and comment.id not in already_done:
-						print "Match! Replying to " + comment.author.name
-						try:
-							reply = comment.reply(reply_text) #TODO: implement buffer, and check for ratelimit exceded
-						except praw.errors.RateLimitExceeded as error:
-							print "Waiting " + str(error.sleep_time) + " seconds due to rate limit..."
-							time.sleep(error.sleep_time)
-							print "Trying again..."
-							reply = comment.reply(reply_text)
-						already_done.add(comment.id)
-						print "Done. See the comment here: " + reply.permalink
-						reply_count += 1
-						print "Bot count: " + str(reply_count)
-			last_checked[subreddit] = first_comment
+				update_config()
+				f = open("config","w+")
+				f.write(json.dumps(config))
+				f.close()
 
-		update_config()
-		f = open("config","w+")
-		f.write(json.dumps(config))
-		f.close()
+				time.sleep(30) #Bot runs every 30 seconds
+			except KeyboardInterrupt:
+				print("")
+				break
+			except:
+				e = sys.exc_info()[0]
+				print("Error: %s" % e)
+				print("Trying again...")
 
-		time.sleep(30) #Bot runs every 30 seconds
-
-	print "Exiting."
+		print("Exiting.")
 
 if len(sys.argv) == 1:
 	bot_loop()
